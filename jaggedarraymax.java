b65753f7b2622707b1e104fc2a32189562aa1e42
karan joshi <kjkaranjoshi2006@gmail.com>
2025-03-13 09:58:25 +0530

    MAXIMUM ELEMENT IN THE JAGGED ARRAY

---
+++ jaggedarraymax.java
@@ -0,0 +1,19 @@
+public class jaggedarraymax {
+    public static void main(String[]args){
+        int [][] jaggedarraymax = {
+            {3,5,9},
+            {1,11},
+            {2,3,7,4}
+        };
+        int max = jaggedarraymax[0][0];
+
+        for (int i = 0; i < jaggedarraymax.length; i++) {
+            for (int j = 0; j < jaggedarraymax[i].length; j++) {
+                if (jaggedarraymax[i][j] > max) {
+                    max = jaggedarraymax[i][j];
+                }
+            }
+        }
+        System.out.println("THE MAXIMUM ELEMENT IS" +max);
+    }
+}
