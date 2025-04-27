47e3561ffaebbeb0fe6f269c2413fbb7d660b646
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2025-04-27 12:20:43 +0530

    Delete shiftoperator.java

--- shiftoperator.java
+++
@@ -1,7 +0,0 @@
-public class shiftoperator {
-    public static void main(String args[]){
-        int x = 8;
-       System.out.println(x << 2 );   //LEFT SHIFT (8*2^2 = 32)
-       System.out.println(x >> 2 );   //RIght shift (8\2^2 = 2 )   
-    }
-}
