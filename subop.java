c339e795694fce980c6da18686d1f710e165b966
Karan_Joshi <kjkaranjoshi2006@gmail.com>
2025-04-27 12:23:44 +0530

    Delete subop.java

--- subop.java
+++
@@ -1,7 +0,0 @@
-class subop{
-    public static void main(String args[]){
-        int x = 10;
-        int y = 5;
-        System.out.println("\n the subtraction of two numbers "+x+" & "+y+" is " +(x-y));
-    }
-}
